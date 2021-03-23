f819dfda753068cd3953a84f7b9f9675ede5ad0d
edwinjoshua2003 <edwinjoshua2003@gmail.com>
2021-03-23 15:12:09 +0530

    Changes to result

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function main(element)
 {
     c++;
     const t = document.getElementById(this.id);
-    console.log(cell);
+    // console.log(cell);
     if(c % 2 == 0){
         t.innerHTML = o.outerHTML;
         cell[parseInt(this.id[1])] = o;
@@ -31,14 +31,16 @@ function main(element)
     }
     t.removeEventListener('click', main);
     check();
-    if(c==9)
+    if(c==9 && !check())
     {
-        document.getElementById('result').innerHTML="✖"+o.outerHTML+"  DRAW";
+        let x = '<p style="font-size:150px;line-height:160px">✖'+o.outerHTML+'</p>';
+        document.getElementById('result').innerHTML=x+"DRAW";
+        end();
     }
 }
 
 function check(){
-    console.log("I'm IN")
+    // console.log("I'm IN")
     let win = {1:((cell[1] == cell[2]) && (cell[2]==cell[3])),
                 2:((cell[4] == cell[5]) && (cell[5]==cell[6])),
                 3:((cell[7] == cell[8]) && (cell[8]==cell[9])), 
@@ -47,38 +49,42 @@ function check(){
                 6:((cell[3] == cell[6]) && (cell[6]==cell[9])),
                 7:((cell[1] == cell[5]) && (cell[5]==cell[9])),
                 8:((cell[3] == cell[5]) && (cell[5]==cell[7]))};
-    console.log(win[1]);
-    console.log(win[2]);
-    console.log(win[3]);
-    console.log(win[4]);
-    console.log(win[5]);
-    console.log(win[6]);
-    console.log(win[7]);
-    console.log(win[8]);
+    // console.log(win[1]);
+    // console.log(win[2]);
+    // console.log(win[3]);
+    // console.log(win[4]);
+    // console.log(win[5]);
+    // console.log(win[6]);
+    // console.log(win[7]);
+    // console.log(win[8]);
     if(win[1] || win[2] || win[3] || win[4] || win[5] || win[6] || win[7] || win[8])
     {
         console.log("Win");
         if(c % 2 == 0){
-            o.setAttribute("font-size", "150px")
-            document.getElementById('result').innerHTML=o.outerHTML + "  is the WINNER!";
+            o.style.setProperty('font-size', "150px");
+            document.getElementById('result').innerHTML=o.outerHTML + "<br>WINNER!";
+            end();
         }
         else{
-            sty = {
-                "font-size": "150px"
-            };
-            x.style.before = sty;
-            document.getElementById('result').innerHTML="✖  is the WINNER!";
+            let x = '<p style="font-size:150px;line-height:160px">✖</p>';
+            document.getElementById('result').innerHTML=x + "WINNER!";
+            end();
         }
         for(let i=1 ; i<10 ; i++)
         {
             document.getElementById(`c${i}`).removeEventListener("click", main);
         }
     }
+    return win[1] || win[2] || win[3] || win[4] || win[5] || win[6] || win[7] || win[8];
+}
+
+function end(){
+    document.getElementById("table").style.setProperty("display", "none");
 }
 
 for(let i=1 ; i<10 ; i++)
 {
     cell[i] = document.getElementById(`c${i}`);
-    console.log(cell[i]);
+    // console.log(cell[i]);
     cell[i].addEventListener("click", main);
 }
